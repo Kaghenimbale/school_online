177695c3909fd675a78cd2a88e98e89e991d85d9
Kagheni Mbale <kaghenimbale@gmail.com>
2024-08-22 12:45:25 +0200

    Add icons to sign input

--- app/components/SignIn.tsx
+++ app/components/SignIn.tsx
@@ -2,6 +2,8 @@
 
 import React, { useState } from "react";
 import { FaUserCircle } from "react-icons/fa";
+import { BiSolidUser } from "react-icons/bi";
+import { IoMdUnlock } from "react-icons/io";
 
 const SignIn = () => {
   const [inputValue, setInputValue] = useState({ email: "", password: "" });
@@ -15,7 +17,7 @@ const SignIn = () => {
     <div className="flex items-center justify-center h-[100vh]">
       <div className="w-fit">
         <div className="flex -mb-3 z-10 justify-center">
-          <FaUserCircle className="flex text-8xl text-slate-300" />
+          <FaUserCircle className="flex text-8xl text-slate-400" />
         </div>
         <form
           action=""
@@ -27,22 +29,32 @@ const SignIn = () => {
               LOGIN
             </h2>
           </div>
-          <input
-            onChange={handleChange}
-            value={inputValue.email}
-            type="email"
-            name="email"
-            placeholder="Email"
-            className="flex py-3 px-4 w-[350px]"
-          />
-          <input
-            onChange={handleChange}
-            value={inputValue.password}
-            type="password"
-            name="password"
-            placeholder="PassWord"
-            className="flex py-3 px-4 w-[350px]"
-          />
+          <label htmlFor="email" className="flex items-center">
+            <div className="p-[0.57rem] bg-white">
+              <BiSolidUser className="text-3xl text-slate-400" />
+            </div>
+            <input
+              onChange={handleChange}
+              value={inputValue.email}
+              type="email"
+              name="email"
+              placeholder="Email"
+              className="flex py-3 px-4 w-[300px] outline-none"
+            />
+          </label>
+          <label htmlFor="password" className="flex items-center">
+            <div className="p-[0.57rem] bg-white">
+              <IoMdUnlock className="text-3xl text-slate-400" />
+            </div>
+            <input
+              onChange={handleChange}
+              value={inputValue.password}
+              type="password"
+              name="password"
+              placeholder="PassWord"
+              className="flex py-3 px-4 w-[300px] outline-none"
+            />
+          </label>
           <label htmlFor="checkbox" className="flex items-center gap-1">
             <input type="checkbox" placeholder="remember me" name="checkbox" />
             remember me
